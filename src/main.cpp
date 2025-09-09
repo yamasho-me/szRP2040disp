287ffd36b02d8ba14748d6bec5498d9e5d8692c5
GomiHgy <h.furukawa1@gmail.com>
2025-09-09 09:53:33 +0800

    各種ライブラリを変更

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <NeoPixelConnect.h>
+#include <Adafruit_NeoPixel.h>
 
 // GPIO0: SDA
 // GPIO1: SCL
 // GPIO2: LED(WS2812)
 
-#define PIN_LED 2
+#define PIN_LEDS 2
 #define PIN_BTN0 5 // adjust to actual design
 
 // RP2040connect
@@ -19,32 +19,46 @@
 
 // using I2C on RP2040
 // https://qiita.com/totuto/items/81944426dc81ab4b7e57
-MbedI2C i2cOLED(p0, p1); // I2C SDA:GP0, SCL:GP1 
-NeoPixelConnect p(2, 2); // GPIO2, 2 pixels
+#define NUM_PIXELS 2 // 2 pixels
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, PIN_LEDS, NEO_GRB + NEO_KHZ800);
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define OLED_RESET -1    // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3c
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &i2cOLED, OLED_RESET);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #include "OLEDdraw.inc"
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
-	pinMode(PIN_BTN0, INPUT_PULLUP);
+  pinMode(PIN_BTN0, INPUT_PULLUP);
 
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed")); for(;;);
+  // I2C SDA:GP0, SCL:GP1
+  Wire.setSDA(0);
+  Wire.setSCL(1);
+  Wire.begin();
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+  {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;
   }
+
+  // init leds
+  strip.begin();
+  strip.clear();
+  strip.show();
 
   // Show initial display buffer contents on the screen --
   // the library initializes this with an Adafruit splash screen.
   display.display();
   delay(2000); // Pause for 2 seconds
 
-	// Draw a single pixel in white
-  //display.drawPixel(10, 10, SSD1306_WHITE);
+  // Draw a single pixel in white
+  // display.drawPixel(10, 10, SSD1306_WHITE);
 
   // Show the display buffer on the screen. You MUST call display() after
   // drawing commands to make them visible on screen!
@@ -52,42 +66,55 @@ void setup() {
   // unless that's what you want...rather, you can batch up a bunch of
   // drawing operations and then update the screen all at once by calling
   // display.display(). These examples demonstrate both approaches...
-/*
-  testdrawline();      // Draw many lines
-  testdrawrect();      // Draw rectangles (outlines)
-  testfillrect();      // Draw rectangles (filled)
-  testdrawcircle();    // Draw circles (outlines)
-  testfillcircle();    // Draw circles (filled)
-  testdrawroundrect(); // Draw rounded rectangles (outlines)
-  testfillroundrect(); // Draw rounded rectangles (filled)
-  testdrawtriangle();  // Draw triangles (outlines)
-  testfilltriangle();  // Draw triangles (filled)
-  testdrawchar();      // Draw characters of the default font
-  testdrawstyles();    // Draw 'stylized' characters
-  testscrolltext();    // Draw scrolling text
-  testdrawbitmap();    // Draw a small bitmap image
-  // Invert and restore display, pausing in-between
-  display.invertDisplay(true);
-  delay(1000);
-  display.invertDisplay(false);
-//  delay(1000);
-//  testanimate(logo_bmp, LOGO_WIDTH, LOGO_HEIGHT); // Animate bitmaps
-*/
+  /*
+    testdrawline();      // Draw many lines
+    testdrawrect();      // Draw rectangles (outlines)
+    testfillrect();      // Draw rectangles (filled)
+    testdrawcircle();    // Draw circles (outlines)
+    testfillcircle();    // Draw circles (filled)
+    testdrawroundrect(); // Draw rounded rectangles (outlines)
+    testfillroundrect(); // Draw rounded rectangles (filled)
+    testdrawtriangle();  // Draw triangles (outlines)
+    testfilltriangle();  // Draw triangles (filled)
+    testdrawchar();      // Draw characters of the default font
+    testdrawstyles();    // Draw 'stylized' characters
+    testscrolltext();    // Draw scrolling text
+    testdrawbitmap();    // Draw a small bitmap image
+    // Invert and restore display, pausing in-between
+    display.invertDisplay(true);
+    delay(1000);
+    display.invertDisplay(false);
+  //  delay(1000);
+  //  testanimate(logo_bmp, LOGO_WIDTH, LOGO_HEIGHT); // Animate bitmaps
+  */
 }
 
-void loop() {
-//	p.neoPixelFill(5, 0, 0, true); // 0=Red, 1=Green?
-//	p.neoPixelSetValue(0, 5, 0, 0, true); // 0=Red
-	p.neoPixelSetValue(0, 0, 0, 5, true); // 0=Blue
-//	p.neoPixelSetValue(1, 5, 0, 0, true); // 1=Green
-	delay(500);
-	p.neoPixelClear();
-	delay(500);
+void loop()
+{
+  // strip.Color(R, G, B)
+  strip.fill(strip.Color(5, 0, 0)); // show RED
+  strip.show();
+  delay(500);
 
-/*
-	// test for BTN
-	if (digitalRead(PIN_BTN0) == 0) p.neoPixelFill(0, 5, 0, true);
-	else p.neoPixelClear();
-	delay(100);
-*/
+  strip.fill(strip.Color(0, 5, 0)); // show GREEN
+  strip.show();
+
+  delay(500);
+
+  strip.fill(strip.Color(0, 0, 5)); // show BLUE
+  strip.show();
+
+  delay(500);
+
+  strip.clear(); // led off
+  // fill_solid(leds, NUM_PIXELS, CRGB::BLACK);
+  strip.show();
+  delay(500);
+
+  /*
+    // test for BTN
+    if (digitalRead(PIN_BTN0) == 0) p.neoPixelFill(0, 5, 0, true);
+    else p.neoPixelClear();
+    delay(100);
+  */
 }
